84f6046a9c204defe3716b969566060c994300e4
Flynn <flynn@datawire.io>
2017-02-28 15:22:17 -0500

    Separate non-SDS and SDS steps.

--- edge-envoy/up.sh
+++ edge-envoy/up.sh
@@ -1,6 +1,8 @@
 DIR=$(dirname $0)
 
 docker build -t edge-envoy:step2 ${DIR}
+# docker tag edge-envoy:step2 dwflynn/edge-envoy:step2
+# docker push dwflynn/edge-envoy:step2
 
 kubectl create -f ${DIR}/deployment.yaml
 kubectl create -f ${DIR}/service.yaml
